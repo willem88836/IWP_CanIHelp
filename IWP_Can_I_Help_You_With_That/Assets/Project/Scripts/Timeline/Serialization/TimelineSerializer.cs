dc43ee2a0104638c1e9fbdc85dadb39c49e01b2f
Willem Meijer <willem1998m@hotmail.com>
2018-11-05 16:48:03 +0100

    documentation

--- IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Timeline/Serialization/TimelineSerializer.cs
+++ IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Timeline/Serialization/TimelineSerializer.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace IWPCIH.EventTracking
 {
+	/// <summary>
+	///		Serializes and deserializes Timeline
+	/// </summary>
 	public static class TimelineSerializer
 	{
-		const char CHAPTERSPACER = '\n';
-		const char EVENTSPACER = (char)1000;
+		private const char CHAPTERSPACER = '\n';
+		private const char EVENTSPACER = 'Ϩ';
 
 
+		/// <summary>
+		///		Serializes the provided Timeline.
+		/// </summary>
 		public static string Serialize(Timeline timeline)
 		{
 			string data = "";
@@ -33,33 +38,39 @@ namespace IWPCIH.EventTracking
 			return data;
 		}
 
-
-
+		/// <summary>
+		///		Deseralizes the provided serialized timeline.
+		/// </summary>
 		public static Timeline Deserialize(string s_Timeline)
 		{
 			Timeline timeline = new Timeline();
 
+			// Splits the data file into chapters. 
 			string[] s_Chapters = s_Timeline.Split(CHAPTERSPACER);
 
 			foreach (string s_Chapter in s_Chapters)
 			{
+				// Splits the chapter into data pieces.
 				string[] vars = s_Chapter.Split(EVENTSPACER);
 
 				if (vars.Length == 0)
 					continue;
 
-				TimelineChapter chapter = new TimelineChapter(vars[0]);
+				// A new chapter is created.
+				string videoName = vars[0];
+				TimelineChapter chapter = new TimelineChapter(videoName);
 
+				// Starts at 1 to skip VideoName.
 				for (int i = 1; i < vars.Length; i++)
 				{
-					TimelineEvent timelineEvent = JsonUtility.FromJson<TimelineEvent>(vars[i]);
+					// converts the var into a TimelineEvent
+					string s_event = vars[i];
+					TimelineEvent timelineEvent = JsonUtility.FromJson<TimelineEvent>(s_event);
 
 					if (timelineEvent == null)
 						continue;
 
 					chapter.AddEvent(timelineEvent);
-
-					Debug.Log(timelineEvent.InvokeTime.ToString());
 				}
 
 				timeline.AddChapter(chapter);
