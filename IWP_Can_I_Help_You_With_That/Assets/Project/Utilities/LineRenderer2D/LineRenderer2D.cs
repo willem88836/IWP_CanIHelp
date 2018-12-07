80d2067c807154ff44b09ae0bf5e7055636e5270
Willem Meijer <willem1998m@hotmail.com>
2018-12-07 14:22:25 +0100

    added first version of linerendering.

---
+++ IWP_Can_I_Help_You_With_That/Assets/Project/Utilities/LineRenderer2D/LineRenderer2D.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+namespace Framework.Core
+{
+	public class LineRenderer2D : MonoBehaviour
+	{
+		public Vector4[] Path;
+		public Color Color;
+		public AnimationCurve Curve;
+
+
+		private void Start()
+		{
+			LineRenderer2DController.Instance.Add(this);
+		}
+	}
+}
---
+++ IWP_Can_I_Help_You_With_That/Assets/Project/Utilities/LineRenderer2D/LineRenderer2DController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Framework.Core
+{
+	[RequireComponent(typeof(RawImage))]
+	[RequireComponent(typeof(RectTransform))]
+	public class LineRenderer2DController : MonoBehaviour
+	{
+		public static LineRenderer2DController Instance;
+
+		public ComputeShader LineRenderShader;
+
+		private RenderTexture targetTexture;
+		private List<LineRenderer2D> lineRenderer2Ds = new List<LineRenderer2D>();
+
+
+		private void Awake()
+		{
+			if (Instance != null)
+				Destroy(Instance.gameObject);
+			Instance = this;
+
+
+			Rect rect = GetComponent<RectTransform>().rect;
+			targetTexture = new RenderTexture((int)(rect.width), (int)(rect.height), 24);
+			targetTexture.name = "LineRenderer2DController_Output";
+			targetTexture.enableRandomWrite = true;
+			targetTexture.Create();
+
+			GetComponent<RawImage>().texture = targetTexture;
+		}
+
+		private void Update()
+		{
+			if (!targetTexture || !LineRenderShader)
+				return;
+
+
+			int threadGroupsX = targetTexture.width / 8;
+			int threadGroupsY = targetTexture.height / 8;
+
+			// Clears render image.
+			int clear = LineRenderShader.FindKernel("Clear");
+			LineRenderShader.SetTexture(clear, "Result", targetTexture);
+			LineRenderShader.Dispatch(clear, threadGroupsX, threadGroupsY, 1);
+
+			// Renders lines on renderimage.
+			int kernel = LineRenderShader.FindKernel("CSMain");
+			LineRenderShader.SetTexture(kernel, "Result", targetTexture);
+			LineRenderShader.SetInt("Iterations", lineRenderer2Ds.Count);
+			foreach (LineRenderer2D renderer in lineRenderer2Ds)
+			{
+				if (renderer.Path == null || renderer.Path.Length == 0)
+					continue;
+
+				// Draws one line onto the texture.
+				LineRenderShader.SetVectorArray("Path", renderer.Path);
+				LineRenderShader.SetVector("Color", renderer.Color);
+				LineRenderShader.Dispatch(kernel, threadGroupsX, threadGroupsY, 1);
+			}
+		}
+
+
+		public void Add(LineRenderer2D renderer)
+		{
+			if (!lineRenderer2Ds.Contains(renderer))
+				lineRenderer2Ds.Add(renderer);
+		}
+	}
+}
