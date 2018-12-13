9820582c38613bb525e3c7033beb7d2b9963214e
Willem Meijer <willem1998m@hotmail.com>
2018-12-13 15:58:55 +0100

    #wip Custom OVR controller script implementation for pointing.

---
+++ IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Features/OVR_Extentions/OVRPointer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///		This is basically a simplified version of Oculus' OVRGrabber.
+/// </summary>
+public class OVRPointer : MonoBehaviour
+{
+	public OVRInput.Controller ControllerType;
+
+	private Vector3 originPosition;
+
+	private void Awake()
+	{
+		// TODO: continue here with Controller stuff. 
+		originPosition = transform.position;
+		originRotation = transform.rotation;
+	}
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 handPosition = OVRInput.GetLocalControllerPosition(ControllerType);
+		Quaternion handRotation = OVRInput.GetLocalControllerRotation(ControllerType);
+
+		transform.gameObject
+	}
+}
