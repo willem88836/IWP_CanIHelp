b00b79d01adb8ca9bb3f850c956f0f1aafdbf13b
Willem Meijer <willem1998m@hotmail.com>
2018-12-19 18:44:11 +0100

    #wip zipping and unzipping updates.

--- IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Features/SaveLoad/TimelineSaveLoad.cs
+++ IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Features/SaveLoad/TimelineSaveLoad.cs
@@ -3,7 +3,8 @@ using IWPCIH.EventTracking;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using Ionic.Zip;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
 
 namespace IWPCIH.Storage
 {
@@ -18,7 +19,9 @@ namespace IWPCIH.Storage
 		// DON'T YOU DARE CHANGE THIS PATHS WHEN YOU DON'T KNOW WHAT YOU'RE DOING. IT CAN KILL YOU PC.
 		public static string BuildExtractPath { get { return Path.Combine(Application.temporaryCachePath, "Build/"); } }
 		public static string SoftSavePath { get { return Path.Combine(Application.temporaryCachePath, "TimelineSaveData/"); } }
-		public static string HardSavePath { get; private set; } 
+		public static string HardSavePath { get; private set; }
+
+		private const string DEFAULTPASSWORD = "*uO5LtEe!n6CwGfuEVa5YukaFRs9OlgRGVByjUCuC@gkZG0MM$cJhk!cPSHR&mdxa";
 
 
 		/// <summary>
@@ -42,6 +45,7 @@ namespace IWPCIH.Storage
 			string s_timeline = SaveLoad.Load(saveName);
 			Timeline tl = TimelineSerializer.Deserialize(s_timeline);
 			timeline.Chapters = tl.Chapters;
+
 		}
 
 		
@@ -83,7 +87,9 @@ namespace IWPCIH.Storage
 			// All video files. 
 			timeline.ForEach((TimelineChapter c) => 
 			{
-				string target = Path.Combine(BuildExtractPath, Path.GetFileName(c.VideoName));
+				// HACK: for some reason the first letter of each file is removed. This kinda solves it, but is ugly AF.
+				string videoName = Path.GetFileName(c.VideoName);
+				string target = Path.Combine(BuildExtractPath, videoName[0] + videoName);
 				File.Copy(c.VideoName, target);
 			});
 
@@ -91,32 +97,145 @@ namespace IWPCIH.Storage
 			string zipName = Path.ChangeExtension((Path.Combine(HardSavePath, timeline.Name)), PROJECTEXTENTION);
 			if (File.Exists(zipName))
 				File.Delete(zipName);
-			
-			ZipFile zip = new ZipFile(zipName);
-			zip.AddDirectory(BuildExtractPath);
 
-			zip.Save();
-			zip.Dispose();
+
+			string outPathname = Path.ChangeExtension((Path.Combine(HardSavePath, timeline.Name)), PROJECTEXTENTION);
+			string password = DEFAULTPASSWORD;
+			string folderName = BuildExtractPath;
+
+			FileStream fsOut = File.Create(outPathname);
+			ZipOutputStream zipStream = new ZipOutputStream(fsOut);
+
+			zipStream.SetLevel(3); //0-9, 9 being the highest level of compression
+
+			zipStream.Password = password;  // optional. Null is the same as not setting. Required if using AES.
+
+			// This setting will strip the leading part of the folder path in the entries, to
+			// make the entries relative to the starting folder.
+			// To include the full path for each entry up to the drive root, assign folderOffset = 0.
+			int folderOffset = folderName.Length + (folderName.EndsWith("\\") ? 0 : 1);
+
+			CompressFolder(folderName, zipStream, folderOffset);
+
+			zipStream.IsStreamOwner = true; // Makes the Close also Close the underlying stream
+			zipStream.Close();
+
+
+
+
 
 			SaveLoad.CleanPath(BuildExtractPath);
 		}
+		
+		// Recurses down the folder structure
+		 //
+		private void CompressFolder(string path, ZipOutputStream zipStream, int folderOffset)
+		{
 
-		// TODO: add error reference when no file exists. 
-		/// <summary>
-		///		Loads a compiled project.
-		/// </summary>
+			string[] files = Directory.GetFiles(path);
+
+			foreach (string filename in files)
+			{
+
+				FileInfo fi = new FileInfo(filename);
+
+				string entryName = filename.Substring(folderOffset); // Makes the name in zip based on the folder
+				entryName = ZipEntry.CleanName(entryName); // Removes drive from name and fixes slash direction
+				ZipEntry newEntry = new ZipEntry(entryName);
+				newEntry.DateTime = fi.LastWriteTime; // Note the zip format stores 2 second granularity
+
+				// Specifying the AESKeySize triggers AES encryption. Allowable values are 0 (off), 128 or 256.
+				// A password on the ZipOutputStream is required if using AES.
+				//   newEntry.AESKeySize = 256;
+
+				// To permit the zip to be unpacked by built-in extractor in WinXP and Server2003, WinZip 8, Java, and other older code,
+				// you need to do one of the following: Specify UseZip64.Off, or set the Size.
+				// If the file may be bigger than 4GB, or you do not need WinXP built-in compatibility, you do not need either,
+				// but the zip will be in Zip64 format which not all utilities can understand.
+				//   zipStream.UseZip64 = UseZip64.Off;
+				newEntry.Size = fi.Length;
+
+				zipStream.PutNextEntry(newEntry);
+
+				// Zip the file in buffered chunks
+				// the "using" will close the stream even if an exception occurs
+				byte[] buffer = new byte[4096];
+				using (FileStream streamReader = File.OpenRead(filename))
+				{
+					StreamUtils.Copy(streamReader, zipStream, buffer);
+				}
+				zipStream.CloseEntry();
+			}
+			string[] folders = Directory.GetDirectories(path);
+			foreach (string folder in folders)
+			{
+				CompressFolder(folder, zipStream, folderOffset);
+			}
+		}
+
+
+
 		public void HardLoad(ref Timeline timeline, string path)
 		{
-			Debug.LogFormat("Attempting to load file from: ({0})", path);
+			Debug.LogFormat("Attempting to load file from: ({0}) to ({1})", path, SoftSavePath);
 
 			if (!Directory.Exists(SoftSavePath))
 				Directory.CreateDirectory(SoftSavePath);
 			else
 				SaveLoad.CleanPath(SoftSavePath);
 
-			ZipFile zip = ZipFile.Read(path);
-			zip.ExtractAll(SoftSavePath, ExtractExistingFileAction.OverwriteSilently);
-			zip.Dispose();
+
+			string archiveFilenameIn = path;
+			string password = DEFAULTPASSWORD;
+			string outFolder = SoftSavePath;
+
+			ZipFile zf = null;
+
+			try
+			{
+				FileStream fs = File.OpenRead(archiveFilenameIn);
+				zf = new ZipFile(fs);
+				if (!string.IsNullOrEmpty(password))
+				{
+					zf.Password = password;     // AES encrypted entries are handled automatically
+				}
+				foreach (ZipEntry zipEntry in zf)
+				{
+					if (!zipEntry.IsFile)
+					{
+						continue;           // Ignore directories
+					}
+					string entryFileName = zipEntry.Name;
+					// to remove the folder from the entry:- entryFileName = Path.GetFileName(entryFileName);
+					// Optionally match entrynames against a selection list here to skip as desired.
+					// The unpacked length is available in the zipEntry.Size property.
+
+					byte[] buffer = new byte[4096];     // 4K is optimum
+					Stream zipStream = zf.GetInputStream(zipEntry);
+
+					// Manipulate the output filename here as desired.
+					string fullZipToPath = Path.Combine(outFolder, entryFileName);
+					string directoryName = Path.GetDirectoryName(fullZipToPath);
+					if (directoryName.Length > 0)
+						Directory.CreateDirectory(directoryName);
+
+					// Unzip file in buffered chunks. This is just as fast as unpacking to a buffer the full size
+					// of the file, but does not waste memory.
+					// The "using" will close the stream even if an exception occurs.
+					using (FileStream streamWriter = File.Create(fullZipToPath))
+					{
+						StreamUtils.Copy(zipStream, streamWriter, buffer);
+					}
+				}
+			}
+			finally
+			{
+				if (zf != null)
+				{
+					zf.IsStreamOwner = true; // Makes close also shut the underlying stream
+					zf.Close(); // Ensure we release resources
+				}
+			}
 
 			SoftLoad(ref timeline, Path.GetFileNameWithoutExtension(path));
 
