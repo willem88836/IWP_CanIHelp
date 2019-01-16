4b7567d6cc97305a006e77fea598351199be71a6
Willem Meijer <willem1998m@hotmail.com>
2019-01-16 17:30:20 +0100

    made multiplechoice menu fully functional

--- IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Features/Events/Variants/MultipleChoiceMenu.cs
+++ IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Features/Events/Variants/MultipleChoiceMenu.cs
@@ -1,37 +1,65 @@
 ﻿using IWPCIH.EventTracking;
+using IWPCIH.VRMenu;
 using System;
+using UnityEngine;
 
 namespace IWPCIH.TimelineEvents
 {
+	[RequireComponent(typeof(GridLayoutGroup3D))]
 	public class MultipleChoiceMenu : BaseEvent
 	{
 		public class MultipleChoiceData : TimelineEventData
 		{
 			public string Question = "";
 			public string[] Answers = new string[0];
+			public string[] Chapters = new string[0];
 		}
 		public override Type EventType { get { return typeof(MultipleChoiceData); } }
 
 
+		public MultipleChoiceOption QuestionPrefab;
 		public MultipleChoiceOption ChoicePrefab;
+		public float CameraDistance;
+
+		private MultipleChoiceData myData;
 
 
 		public override void Invoke()
 		{
-			MultipleChoiceData myData = (MultipleChoiceData)Event;
+			myData = (MultipleChoiceData)Event;
 
-			Spawn(myData.Question);
+			MultipleChoiceOption question = Instantiate(QuestionPrefab, transform);
+			question.SetText(myData.Question);
 
-			foreach (string answer in myData.Answers)
+			for (int i = 0; i < myData.Answers.Length; i++)
 			{
-				Spawn(answer);
+				string answer = myData.Answers[i];
+				MultipleChoiceOption option = Instantiate(ChoicePrefab, transform);
+				option.SetText(answer);
+				option.Index = i;
+				option.OnClick += OnClick;
 			}
+
+			GridLayoutGroup3D layoutGroup3D = GetComponent<GridLayoutGroup3D>();
+			transform.position = transform.position + (Vector3.up * layoutGroup3D.Spacing.Y * myData.Answers.Length / 2);
+			Camera main = Camera.main;
+			transform.position += main.transform.forward * CameraDistance;
+			transform.rotation = main.transform.rotation;
+
+			(TimelineController.Instance as TimelineExecuter).TogglePause(true);
 		}
 
-		private void Spawn(string text)
+		public void OnClick(int index)
 		{
-			MultipleChoiceOption option = Instantiate(ChoicePrefab, transform);
-			option.SetText(text);
+			(TimelineController.Instance as TimelineExecuter).TogglePause(false);
+			int chapterId = TimelineController.Instance.CurrentTimeline.GetIdOf(myData.Chapters[index]);
+
+			if (chapterId != -1)
+			{
+				TimelineController.Instance.SwitchChapterTo(chapterId);
+			}
+
+			Destroy(gameObject);
 		}
 	}
 }
--- IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Features/Events/Variants/MultipleChoiceOption.cs
+++ IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Features/Events/Variants/MultipleChoiceOption.cs
@@ -1,14 +1,44 @@
-﻿using UnityEngine;
+﻿using Framework.Core;
+using System;
+using UnityEngine;
 
 namespace IWPCIH.TimelineEvents
 {
-	public class MultipleChoiceOption : MonoBehaviour
+	public class MultipleChoiceOption : MonoBehaviour, ISelectable
 	{
 		public TextMesh TextField;
+		[NonSerialized] public int Index = 0;
+
+		public Action<int> OnClick = null;
+
+		public void Select()
+		{
+			OnClick.SafeInvoke(Index);
+		}
 
 		public void SetText(string text)
 		{
 			TextField.text = text;
 		}
+
+
+#if UNITY_EDITOR
+		private void Update()
+		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				RaycastHit info;
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(ray, out info, float.PositiveInfinity))
+				{
+					Debug.DrawLine(ray.origin, info.point, Color.red, 1);
+					if (info.transform == transform)
+					{
+						Select();
+					}
+				}
+			}
+		}
+#endif
 	}
 }
