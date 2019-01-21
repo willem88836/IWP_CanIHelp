fdcbad117fce618b829c9886ca8b8635181065a9
niels-jan <niels-jantimmerman@hotmail.com>
2019-01-21 12:49:35 +0100

    multiple choice menu is ordered in scene.

--- IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Features/Events/Variants/MultipleChoiceMenu.cs
+++ IWP_Can_I_Help_You_With_That/Assets/Project/Scripts/Features/Events/Variants/MultipleChoiceMenu.cs
@@ -1,37 +1,73 @@
 ﻿using IWPCIH.EventTracking;
 using System;
+using UnityEngine;
 
 namespace IWPCIH.TimelineEvents
 {
-	public class MultipleChoiceMenu : BaseEvent
-	{
-		public class MultipleChoiceData : TimelineEventData
-		{
-			public string Question = "";
-			public string[] Answers = new string[0];
-		}
-		public override Type EventType { get { return typeof(MultipleChoiceData); } }
+    public class MultipleChoiceMenu : BaseEvent
+    {
+        public class MultipleChoiceData : TimelineEventData
+        {
+            public string Question = "";
+            public string[] Answers = new string[0];
+        }
+        public override Type EventType { get { return typeof(MultipleChoiceData); } }
 
 
-		public MultipleChoiceOption ChoicePrefab;
+        public MultipleChoiceOption ChoicePrefab;
+        public float Spacing;
 
 
-		public override void Invoke()
-		{
-			MultipleChoiceData myData = (MultipleChoiceData)Event;
+        public override void Invoke()
+        {
 
-			Spawn(myData.Question);
+            (TimelineController.Instance as TimelineExecuter).TogglePause(true);
 
-			foreach (string answer in myData.Answers)
-			{
-				Spawn(answer);
-			}
-		}
+            LocateMenu();
 
-		private void Spawn(string text)
-		{
-			MultipleChoiceOption option = Instantiate(ChoicePrefab, transform);
-			option.SetText(text);
-		}
-	}
+            MultipleChoiceData myData = (MultipleChoiceData)Event;
+
+            Spawn(-1, myData.Question);
+
+            for (int i = 0; i < myData.Answers.Length; i++) 
+            {
+                string answer = myData.Answers[i];
+
+                Spawn(i, answer);
+
+
+            }
+        }
+
+
+        private void LocateMenu()
+        {
+            Vector3 lookDir = Camera.main.transform.forward; //= camera richting
+            Quaternion lookRotation = Camera.main.transform.rotation;
+
+            transform.position = lookDir * 3f;
+            transform.rotation = lookRotation;
+        }
+
+
+        private void Spawn(int index, string text)
+        {
+            MultipleChoiceOption option = Instantiate(ChoicePrefab, transform);
+
+            option.transform.Translate(Vector3.down * index * Spacing);
+
+            option.SetText(text);
+
+
+            // lower object.
+        }
+
+
+
+
+
+
+
+
+    }
 }
